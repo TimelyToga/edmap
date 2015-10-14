517fb4a49d8a7d8eb377dc67692dc91b67459266
TimelyToga <tblumberg39@gmail.com>
2015-10-13 21:59:13 -0400

    Added Genre create form

--- edmap/genre/views.py
+++ edmap/genre/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
+from forms import GenreForm
+from django.http import HttpResponseRedirect
+from django.core.context_processors import csrf
 
 # Create your views here.
 def create(request):
-    return render(request, 'create_genre.html', {})
+    if request.POST:
+        form = GenreForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect('/genre/all')
+    else:
+        form = GenreForm()
+
+    args = {}
+    args.update(csrf(request))
+
+    args['form'] = form
+
+    return render(request, 'create_genre.html', args)
 
 def view_genre(request, genre_name):
     return render(request, 'view_genre.html', {'genre_name': genre_name})
